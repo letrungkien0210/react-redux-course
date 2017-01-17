6d93e02a5b7ac123ad86a1dfb14a1e240921324b
letrungkien0210 <letrungkien0210@gmail.com>
2017-01-17 17:19:37 +0700

    Final Section 3: Redux & jquery

--- 2. Redux & Jquery/v01. conver to Jquery - exercise - solution/index.js
+++ 2. Redux & Jquery/v01. conver to Jquery - exercise - solution/index.js
@@ -4,24 +4,23 @@ var store = Redux.createStore(combineReducer, Redux.applyMiddleware(logger, cras
 // step 1.3
 function render() {
   var state = store.getState();
-  document.getElementById('value').innerHTML = state.count.result;
-  document.getElementById('value2').innerHTML = state.sum;
+  $('#value').text(state.count.result);
+  $('#value2').text(state.sum);
     
   if(state.count.loading){
-    document.getElementById('status').innerHTML = "is loading...";
+    $('#status').text("is loading...");
   }else{
-    document.getElementById('status').innerHTML = "loaded";
+    $('#status').text("loaded");
   }
 
   // image
   document.getElementById('imagesStatus').innerHTML = state.images.loading;
   if(state.images.loading =="loading…"){          
-    document.getElementById('imagesList').innerHTML = "";
+    $('#imagesList').text ("");
   }
   else if(state.images.loading =="loaded"){                  
     for(var i=0; i< state.images.data.length; i++){
-      document.getElementById('imagesList').innerHTML 
-        += ("<img src='"  + state.images.data[i].link + "' style='height:200px'>");
+      $('#imagesList').append("<img src='"  + state.images.data[i].link + "' style='height:200px'>");
     }
   }
 }
